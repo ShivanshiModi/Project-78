94def999bd02a31ed78f8dc458ba30d79b5bad71
ShivanshiModi <87198158+ShivanshiModi@users.noreply.github.com>
2021-08-25 17:48:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var images = 
+["https://i.postimg.cc/MGn9GJXw/family.jpg",
+"Profile_Pic.PNG",
+"Cute Sister.PNG" ,
+"Preety Mother.PNG",
+"Father.PNG"];
+var names = 
+["Fmaily Book",
+"Shivanshi Modi",
+"Shreya Modi",
+"Gauri Modi",
+"Abhishek Modi"];
+
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 4
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+
+      }
+
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i];
+    
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
